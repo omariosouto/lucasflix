af434de860a43185f9764da20637b2dc748b952f
Mario Souto <soutomarios@gmail.com>
2020-07-27 19:24:25 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 
-interface MenuProps {
-  name: string;
-}
-
-const Menu:React.FC<MenuProps> = ({ name }) => {
-  return (
-    <div>
-      Menu: {name}
-    </div>
-  );
-}
-
 function App() {
   return (
-    <div className="App">
-      Lucasflix
+    <main>
+      <img
+        className="logo"
+        alt="Logo"
+        src="https://raw.githubusercontent.com/imersao-alura/aluraflix/master/src/assets/img/Logo.png"
+      />
 
-      <Menu name={'Name'} />
-    </div>
+      <label>
+        <input type="" />
+        <span>Título do Vídeo</span>
+      </label>
+
+      <label>
+        <input type="" />
+        <span>URL do Vídeo</span>
+      </label>
+
+      <button>
+        Cadastrar
+      </button>
+
+
+      <section className="listaDeVideos">
+        <h2>
+          Vídeos cadastrados
+        </h2>
+        <ul>
+          {
+            [1,2,3,4,5,6].map(() => (
+              <li className="card" style={{ backgroundImage: 'https://placehold.it/1920x1080' }}>
+                <article>
+                  <h3 className="titulo">Título do vídeo</h3>
+                </article>
+              </li>
+            ))
+          }
+        </ul>
+      </section>
+    </main>
   );
 }
 
