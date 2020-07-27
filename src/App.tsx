f77a6726b25c77015d2afb7117094770df411803
Mario Souto <soutomarios@gmail.com>
2020-07-27 18:14:51 -0300

    First update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+interface MenuProps {
+  name: string;
+}
+
+const Menu:React.FC<MenuProps> = ({ name }) => {
+  return (
+    <div>
+      Menu: {name}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Lucasflix
+
+      <Menu name={'Name'} />
     </div>
   );
 }
