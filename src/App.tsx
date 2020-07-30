78fe679f7337be0ff7d88fe403c58e79a32d5569
Mario Souto <soutomarios@gmail.com>
2020-07-29 22:17:22 -0300

    Código do fim da live

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+function getYouTubeId(youtubeURL: string) {
+  return youtubeURL.replace(/^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/, '$7');
+}
+
+interface IVideo {
+  url: string;
+  titulo: string;
+}
 
 function App() {
+  const videos: IVideo[] = [
+    {
+      url: 'https://www.youtube.com/watch?v=5MzOCxSWrrc',
+      titulo: 'Recriando o React em 20min',
+    }
+  ];
+
+  const [videosDoState, setVideosNoState] = useState<IVideo[]>([]);
+
   return (
     <main>
       <img
@@ -9,19 +27,32 @@ function App() {
         src="https://raw.githubusercontent.com/imersao-alura/aluraflix/master/src/assets/img/Logo.png"
       />
 
-      <label>
-        <input type="" />
-        <span>Título do Vídeo</span>
-      </label>
+      <form onSubmit={(event) => {
+        event.preventDefault();
 
-      <label>
-        <input type="" />
-        <span>URL do Vídeo</span>
-      </label>
+        setVideosNoState([
+          ...videosDoState,
+          {
+            url: 'https://www.youtube.com/watch?v=5MzOCxSWrrc',
+            titulo: 'Recriando o React em 20min',
+          }
+        ]);
+      }}>
+        <label>
+          <input type="" />
+          <span>Título do Vídeo</span>
+        </label>
 
-      <button>
-        Cadastrar
-      </button>
+        <label>
+          <input type="" />
+          <span>URL do Vídeo</span>
+        </label>
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </form>
+
 
 
       <section className="listaDeVideos">
@@ -30,12 +61,15 @@ function App() {
         </h2>
         <ul>
           {
-            [1, 2, 3, 4, 5, 6].map(() => (
-              <li className="card" style={{ backgroundImage: 'url(https://placehold.it/1920x1080)' }}>
+            videosDoState.map((video) => (
+              <a
+                className="card"
+                href={video.url}
+                style={{ backgroundImage: `url(https://img.youtube.com/vi/${getYouTubeId(video.url)}/maxresdefault.jpg)` }}>
                 <article>
-                  <h3 className="titulo">Título do vídeo</h3>
+                  <h3 className="titulo">{video.titulo}</h3>
                 </article>
-              </li>
+              </a>
             ))
           }
         </ul>
